e591f5059589bb1b0e84bf8b2b9435e4bcddf341
Jonathon Jones <jonathon_d_jones@yahoo.com>
2011-01-05 14:40:41 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoppingOrganizer::Application.config.secret_token = 'cb3a5cd88856bf70ccc2aac317dd0895d971c512538774f897883ca71c96e7b6b6406bb27bc2fbe03586ba8a58dee4802d1b67507f16079e9eb48668bd5fe38d'
