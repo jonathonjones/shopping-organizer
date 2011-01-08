989cb96f324f2fe89d3fcba0ef55000b233cadb4
Jonathon Jones <jonathon_d_jones@yahoo.com>
2011-01-08 11:47:50 -0500

    Adds session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ShoppingOrganizer::Application.routes.draw do
+  match '/auth/:provider/callback', :to => 'sessions#create'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
